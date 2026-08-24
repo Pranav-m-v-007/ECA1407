b4d8a47cab9f6fe43fb1beb36597654dc433243b
Pranav-m-v-007 <222940343+Pranav-m-v-007@users.noreply.github.com>
2026-08-24 13:14:19 +0530

    Add files via upload

---
+++ LAB EXP/Exp 12/main.c
@@ -0,0 +1,20 @@
+#include <reg51.h>
+
+void main()
+{
+    while (1)
+    {
+        unsigned int num1, num2;
+        unsigned long int product;
+
+        num1 = 0x2122;
+        num2 = 0xBBBB;
+
+        product = (unsigned long int)num1 * num2;
+
+        P0 = product & 0xFF;
+        P1 = (product & 0xFF00) >> 8;
+        P2 = (product & 0xFF0000) >> 16;
+        P3 = (product & 0xFF000000) >> 24;
+    }
+}
